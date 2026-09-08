5b1c29340f99b1f317f610a64d9c6a4d94a69605
Martijn Bogaert <martijn.bogaert@sirus.be>
2026-09-08 14:29:17 +0200

    Get rid of https bestuurseenheid uris

--- config/output-graph-mapping.js
+++ config/output-graph-mapping.js
@@ -6,17 +6,9 @@ export const OUTPUT_GRAPH_BY_BESTUURSEENHEID = new Map([
     "http://data.lblod.info/id/bestuurseenheden/353234a365664e581db5c2f7cc07add2534b47b8e1ab87c821fc6e6365e6bef5",
     "http://mu.semte.ch/graphs/public/gent",
   ],
-  [
-    "https://data.lblod.info/id/bestuurseenheden/353234a365664e581db5c2f7cc07add2534b47b8e1ab87c821fc6e6365e6bef5",
-    "http://mu.semte.ch/graphs/public/gent",
-  ],
   // Wingene
   [
     "http://data.lblod.info/id/bestuurseenheden/99ed6eee81a7aca47517cbffb46eaba38f3987eeb4ad32c020898644769eb615",
-    "http://mu.semte.ch/graphs/public/wingene",
-  ],
-  [
-    "https://data.lblod.info/id/bestuurseenheden/99ed6eee81a7aca47517cbffb46eaba38f3987eeb4ad32c020898644769eb615",
     "http://mu.semte.ch/graphs/public/wingene",
   ],
 ]);
